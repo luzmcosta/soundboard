2bb1a0173d7b16450a90c17f426779344236ba1b
David LeMieux <lemieuxster@gmail.com>
2014-05-22 14:44:41 -0700

    Removed client, added list and other

--- public/scripts/main.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    var host = location.origin.replace(/^http/, 'ws');
-    var socket = io.connect(host);
-
-    socket.on('sound', function (msg) {
-        console.log("sound", msg);
-    });
-});
