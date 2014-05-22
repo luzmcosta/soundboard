4c6c19578c68b08bc02aa8dfe67353ff00c932d3
David LeMieux <lemieuxster@gmail.com>
2014-05-22 12:04:22 -0700

    updated server with basic API and S3 creds

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,7 +2,7 @@ $(function() {
     var host = location.origin.replace(/^http/, 'ws');
     var socket = io.connect(host);
 
-    socket.on('connected', function (msg) {
-        console.log("connected in browser");
+    socket.on('sound', function (msg) {
+        console.log("sound", msg);
     });
 });
