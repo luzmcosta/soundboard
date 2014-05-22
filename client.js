c9611813765ae4ff173259bbdd5230b314b3a6a4
David LeMieux <lemieuxster@gmail.com>
2014-05-22 14:44:41 -0700

    Removed client, added list and other

--- client.js
+++
@@ -1,4 +0,0 @@
-//Soundboard Client, it gets messages from the server and uses them to make some noise.
-
-var soundboard_client = require('./lib/soundboard_client');
-soundboard_client.start();
---
+++ public/scripts/server-main.js
@@ -0,0 +1,54 @@
+$(function() {
+    var player = $('#player')[0];
+
+    $.ajax('/sound/list').done(function(list) {
+        var $list = $('#sound-list');
+
+        _.each(list, function(soundInfo) {
+            var sound = $('<p></p>');
+            sound.addClass('sound');
+            sound.html(soundInfo.key);
+            sound.data('key', soundInfo.key);
+            sound.data('url', soundInfo.url);
+
+            $list.append(sound);
+        });
+
+        $list.on('click', '.sound', function() {
+            endPreview();
+            var key = $(this).data('key');
+            $.ajax('/sound/' + key);
+        });
+
+        $list.on('mouseover', '.sound', function() {
+            preview($(this).data('url'));
+        });
+
+        $list.on('mouseout', '.sound', function() {
+            endPreview();
+        });
+    });
+
+    function preview(url) {
+        endPreview();
+        try {
+            player.setAttribute("src", url);
+            player.play();
+        } finally {}
+    }
+
+    function endPreview() {
+        try {
+            player.pause();
+        } finally {}
+    }
+
+    /*
+    $.ajax({
+        url: '/speech',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({text:"", voice:"Alex"})
+    });
+     */
+});
