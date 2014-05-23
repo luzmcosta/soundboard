226aa5a444e9a6f5fd540764b3fbc3fa143a1c1a
David LeMieux <lemieuxster@gmail.com>
2014-05-23 16:16:52 -0700

    Better UI and matching

--- public/scripts/server-main.js
+++ public/scripts/server-main.js
@@ -1,18 +1,21 @@
 $(function() {
     var player = $('#player')[0];
+    var ultralist = null;
+    var mp3Regex = /\.mp3/gi;
+    var playbackTimer = null;
 
-    var $speak = $('#speak');
-    $speak.on('click', function() {
-        var text = $('#speech-text').val();
-        if (text) {
-            $.ajax({
-                url: '/speech',
-                type: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify({text:text, voice:"Victoria"})
-            });
-        }
-    });
+//    var $speak = $('#speak');
+//    $speak.on('click', function() {
+//        var text = $('#speech-text').val();
+//        if (text) {
+//            $.ajax({
+//                url: '/speech',
+//                type: 'POST',
+//                contentType: 'application/json',
+//                data: JSON.stringify({text:text, voice:"Victoria"})
+//            });
+//        }
+//    });
 
     $.ajax('/sound/list').done(function(list) {
         var $list = $('#sound-list');
@@ -20,7 +23,7 @@ $(function() {
         _.each(list, function(soundInfo) {
             var sound = $('<div></div>');
             sound.addClass('sound');
-            sound.html(soundInfo.key);
+            sound.html(soundInfo.key.replace(mp3Regex, ''));
             sound.data('key', soundInfo.key);
             sound.data('url', soundInfo.url);
 
@@ -28,19 +31,80 @@ $(function() {
         });
 
         $list.on('click', '.sound', function() {
-            endPreview();
-            var key = $(this).data('key');
-            $.ajax('/sound/' + key);
+            if (!$(this).data('preview')) {
+                var key = $(this).data('key');
+                $.ajax('/sound/' + key);
+            } else {
+                $(this).data('preview', false);
+            }
         });
 
-        $list.on('mouseover', '.sound', function() {
-            preview($(this).data('url'));
+        $list.on('mousedown', '.sound', function() {
+            var $this = $(this);
+            playbackTimer = setTimeout(function() {
+                $this.data('preview', true);
+                preview($this.data('url'));
+            }, 1000);
         });
 
-        $list.on('mouseout', '.sound', function() {
+        $list.on('mouseup', '.sound', function() {
+            clearTimeout(playbackTimer);
             endPreview();
         });
+
+        var $ultrabox = $('#ultrabox');
+        ultralist = _.map(list, function(item) {
+            return {key: item.key.replace(mp3Regex, '')};
+        });
+
+        $ultrabox.typeahead({
+            minLength: 3,
+            highlight: true,
+            hint: true
+        },{
+            name: 'sounds',
+            displayKey: 'key',
+            source: function(query, callback) {
+                var pattern = new RegExp(query, 'i');
+                var sortedList = _.filter(ultralist, function(item) {
+                    return item.key.match(pattern);
+                });
+
+                callback(sortedList);
+            }
+        });
+
+        $ultrabox.on('typeahead:selected typeahead:autocompleted', function() {
+            /* selected or autocompleted */
+        });
+
+        $ultrabox.on('keypress', function(evt) {
+            if (evt.which === 13) { //ENTER
+                var val = $(this).val();
+                sendRequest(val);
+                $(this).val('');
+                $(this).typeahead('close');
+            }
+        });
+
     });
+
+    function sendRequest(val) {
+        var soundMatch = _.find(ultralist, function(item) {
+            return item.key.toLowerCase() === val.toLowerCase();
+        });
+
+        if (soundMatch) {
+            $.ajax('/sound/' + val + ".mp3");
+        } else if (val) {
+            $.ajax({
+                url: '/speech',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify({text:val, voice:"Victoria"})
+            });
+        }
+    }
 
     function preview(url) {
         endPreview();
